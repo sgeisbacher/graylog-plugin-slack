3d564e85df50c342cb9c6dfdafb164522b79950d
Stefan Geisbacher <s.geisbacher@netconomy.net>
2016-03-13 22:17:34 +0100

    added sources-list to attachment

--- src/main/java/org/graylog2/plugins/slack/callback/SlackAlarmCallback.java
+++ src/main/java/org/graylog2/plugins/slack/callback/SlackAlarmCallback.java
@@ -1,5 +1,8 @@
 package org.graylog2.plugins.slack.callback;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.graylog2.plugin.MessageSummary;
 import org.graylog2.plugins.slack.SlackClient;
 import org.graylog2.plugins.slack.SlackMessage;
 import org.graylog2.plugins.slack.SlackPluginBase;
@@ -12,6 +15,7 @@ import org.graylog2.plugin.configuration.ConfigurationException;
 import org.graylog2.plugin.configuration.ConfigurationRequest;
 import org.graylog2.plugin.streams.Stream;
 
+import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
@@ -47,6 +51,7 @@ public class SlackAlarmCallback extends SlackPluginBase implements AlarmCallback
 
         // Add attachments if requested.
         if(configuration.getBoolean(CK_ADD_ATTACHMENT)) {
+            message.addAttachment(new SlackMessage.AttachmentField("Source(s)", buildSourcesList(result), false));
             message.addAttachment(new SlackMessage.AttachmentField("Stream ID", stream.getId(), true));
             message.addAttachment(new SlackMessage.AttachmentField("Stream Title", stream.getTitle(), false));
             message.addAttachment(new SlackMessage.AttachmentField("Stream Description", stream.getDescription(), false));
@@ -57,6 +62,19 @@ public class SlackAlarmCallback extends SlackPluginBase implements AlarmCallback
         } catch (SlackClient.SlackClientException e) {
             throw new RuntimeException("Could not send message to Slack.", e);
         }
+    }
+
+    private String buildSourcesList(AlertCondition.CheckResult result) {
+        if (result != null && result.getMatchingMessages() != null) {
+            List<String> sources = Lists.newArrayList();
+            for (MessageSummary messageSummary : result.getMatchingMessages()) {
+                if (messageSummary != null) {
+                    sources.add(messageSummary.getSource());
+                }
+            }
+            return StringUtils.join(sources, ",");
+        }
+        return "no sources found";
     }
 
     public String buildMessage(Stream stream, AlertCondition.CheckResult result) {
